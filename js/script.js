80d1610437edfa731b22c5a14c32cf0135d3ad05
Leonardo Zabotto <leozabotto02@gmail.com>
2020-09-18 14:49:37 -0300

    Functional Bin2Dec

--- js/script.js
+++ js/script.js
@@ -22,5 +22,49 @@ function conversor(binary) {
       } 
     }
     return decimal;
-  }  
+  }
+  const decimal = binToDec(binary)
+  return decimal;
 }
+
+function privateScope() {
+
+  function addDigit(e) {
+    const binInput = document.querySelector('#binary');
+    const result = document.querySelector('#response')
+    if(binInput.value.length < 8) {
+      binInput.value += e.srcElement.innerText;
+      result.innerHTML = conversor(Number(binInput.value));
+    }
+    else {
+      M.toast({html: 'Limit of 8 digits reached!'});
+    } 
+  }
+
+  function removeDigit(e) {
+    const binInput = document.querySelector('#binary');
+    const result = document.querySelector('#response')
+    if(!(binInput.value.length)) {
+      M.toast({html: 'No digit to remove!'});
+      result.innerHTML = '';
+    }
+    else {
+      binInput.value = binInput.value.slice(0, -1);
+      result.innerHTML = conversor(Number(binInput.value));
+    } 
+  }
+
+
+  const btn0 = document.querySelector('#btn0');
+  const btn1 = document.querySelector('#btn1');
+  const delBtn = document.querySelector('#removeDgt');
+
+  btn0.addEventListener('click', addDigit);
+  btn1.addEventListener('click', addDigit);
+  delBtn.addEventListener('click', removeDigit);
+ 
+}
+
+privateScope();
+
+
